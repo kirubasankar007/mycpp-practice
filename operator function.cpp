91d88535ca79bc7fc844b7f5523009f64f5e2596
kirubasankar007 <108586777+kirubasankar007@users.noreply.github.com>
2025-06-17 15:25:39 +0530

    Create operator function.cpp

---
+++ operator function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    
+    int a=5,b=2;
+    cout<<a+b<<endl;
+    cout<<a-b<<endl;
+    cout<<a*b<<endl;
+    cout<<a%b<<endl;
+    cout<<a/b<<endl;
+    return 0;
+}
